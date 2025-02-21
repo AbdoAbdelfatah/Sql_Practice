9cc912a706802a726dde0befc7e58dd70ba0cdcc
Abdelrahman Abdelfatah <119816888+AbdoAbdelfatah@users.noreply.github.com>
2025-02-21 16:50:43 +0200

    user defined functions -->(scalar , inline and multistatement)

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,136 +1,84 @@
-/*
-Create the following database using wizard Consists of 2 File Groups 
-{ SeconderyFG (has two data files) and ThirdFG (has two data files) } 
-*/
-USE SD;
-SELECT name, type_desc, data_space_id, physical_name 
-FROM sys.master_files
-WHERE database_id = DB_ID('SD');
+-- 1.Create a scalar function that takes date and returns Month name of that date.
+
+create function getMonth (@d date)
+returns varchar(20)
+	begin
+		declare @month varchar(20);
+		set @month =datename(month,@d);
+		return @month;
+	end 
+
+select dbo.getMonth('2025-03-25');
 
 
+--Create a multi-statements table-valued function that takes 2 integers and returns the values between them.
 
-----------------------------
+create function getNums(@x int ,@y int )
+returns @t table(
+             num int
+                )
+	as 
+	begin 
+	     Declare @stValue int = @x;
+		 while @stValue<=@y
+			begin 
+			    insert into @t values(@stValue);
 
-use SD;
-/*
-1.	Create the Department table 
--Create a new user data type named loc with the following Criteria:
-•	nchar(2)
-•	default:NY 
-•	create a rule for this Datatype :values in (NY,DS,KW)) and associate it to the location column  
-*/
-create rule r1 as @x in ('NY','DS','KW');
-create default d1 as 'NY';
-sp_addtype 'loc' , 'nchar(2)';
+				set @stValue=@stValue+1;
+			end 
+		 return 
+end 
 
-sp_bindrule r1,loc;
-sp_bindefault d1,loc;
+select *from getNums(3,15);
 
-create table Department(
-DeptNo int primary key,
-DeptName varchar(20),
-Location loc
+
+--3.Create inline function that takes Student No and returns Department Name with Student full name.
+
+use iti 
+ 
+create function getStuds(@Sid int)
+returns table 
+as
+return (
+   select s.Dept_Id,s.St_Fname+' '+s.St_Lname as fullName
+   from Student s
+   where s.St_Id=@Sid
 )
 
-
-/*
-Create the Employee table 
-1-Create it by code
-2-PK constraint on EmpNo
-3-FK constraint on DeptNo
-4-Unique constraint on Salary
-5-EmpFname, EmpLname don’t accept null values
-6-Create a rule on Salary column to ensure that it is less than 6000 
-*/
-create table Employee(
-EmpNo  int primary key,
-EmpFname varchar(20) not null,
-EmpLname varchar(20) not null,
-DeptNo int FOREIGN KEY REFERENCES Department(DeptNo),
-Salary int unique,
-);
-
-create rule r2 as @x <6000;
-sp_bindrule r2,'Employee.Salary';
+select * from getStuds(11)
 
 
 /*
-Create the Project table 
-1-ProjectName can't contain null values
-2-Budget allow null
+4.Create a scalar function that takes Student ID and returns a message to user 
+	a.	If first name and Last name are null then display 'First name & last name are null'
+	b.	If First name is null then display 'first name is null'
+	c.	If Last name is null then display 'last name is null'
+	d.	Else display 'First name & last name are not null'
 */
 
-create table Project(
-ProjectNo int primary key,
-ProjectName varchar(50) not null,
-Budget int
-);
+create function checkNam(@Sid int)
+returns varchar(50)
+begin
+    declare @ans varchar(50);
+	declare @f varchar(20),@l varchar(20);
+	select @f=s.St_Fname,@l=s.St_Lname from student s where St_Id=@Sid;
+		if @f is null and @l is null 
+		set @ans ='First name & last name are null'
+		else if @f is null 
+		set @ans ='first name is null'
+		else if @l is null 
+		set @ans ='last name are null'
+		else set @ans ='First name & last name are not null'
+return @ans;
+end 
 
-/*
-Create the Works_on table 
-1- EmpNo INTEGER NOT NULL
-2-ProjectNo doesn't accept null values
-3-Job can accept null
-4-Enter_Date can’t accept null
-and has the current system date as a default value[visually]
-5-The primary key will be EmpNo,ProjectNo) 
-6-there is a relation between works_on and employee, Project  tables
+select dbo.checkNam(14);
+
+
+/* 
+5.Write a query that returns the Student No and Student first name without the last char
 */
 
-create table Works_on(
-EmpNo INTEGER NOT NULL,
-ProjectNo INTEGER NOT NULL,
-Job VARCHAR(50) NULL, 
-Enter_Date DATE NOT NULL DEFAULT GETDATE(),
-constraint c1 primary key(EmpNo,ProjectNo),
-constraint c2 foreign key (ProjectNo) REFERENCES Project(ProjectNo) 
-on delete cascade on update cascade,
-constraint c3 foreign key (EmpNo) REFERENCES Employee(EmpNo) 
-on delete cascade on update cascade,
-)
-
-/*
-1	Create the following schema and transfer the following tables to it 
-a.	Company Schema 
-	i.	Department table (Programmatically)
-	ii.	Project table (using wizard)
-b.	Human Resource Schema
-	i.	  Employee table (Programmatically)
-*/
-
-create schema Company;
-alter schema Company transfer Department; 
-alter schema Company transfer Project; 
-
-create schema HumanResource;	
-alter schema HumanResource transfer Employee;
-
-
--- 2.Create Synonym for table Employee as Emp and
--- then run the following queries and describe the results
-
-create synonym Emp
-for HumanResource.Employee;
-
-Select * from Emp;
-
-
-/*
-Increase the budget of the project where the manager number is 10102 by 10%.
-*/
-
-update Company.Project 
-set Budget += (Budget*0.1)
-from Company.Project p inner join Works_on w
-on p.ProjectNo=w.ProjectNo and w.EmpNo=10102;
-
-
-/*
-Change the name of the department for which the employee named Ann.
-The new department name is Sales.
-*/
-
-update d
-set d.DeptName='Sales'
-from Emp e inner join Company.Department d
-on d.DeptNo=e.DeptNo and e.EmpFname ='Ann';
+SELECT s.St_Id, LEFT(s.St_Fname, LEN(s.St_Fname) - 1) AS FirstNameWithoutLastChar
+FROM Student s
+WHERE LEN(s.St_Fname) > 1;
